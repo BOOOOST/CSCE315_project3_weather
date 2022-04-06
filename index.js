52ece95e39f8746762617fcbeec12b4c6e60de87
hamzasyed <hamzasyed@tamu.edu>
2022-04-06 13:51:31 -0500

    update date selector to limit range

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function getCurrentDate(){
+  //https://stackoverflow.com/questions/32378590/
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {dd = '0' + dd;}
+
+  if (mm < 10) {mm = '0' + mm;} 
+    
+  today = yyyy + '-' + mm + '-' + dd;
+  return today;
+}
+
+function setMinEndDate(){
+  console.log("update min end day");
+  let minEndDay = document.getElementById("startDate").value;
+  document.getElementById("endDate").setAttribute("min", minEndDay);
+  document.getElementById("endDate").setAttribute("value", minEndDay);
+  console.log(minEndDay);
+  return minEndDay;
+}
+
+function setCountryCode(){
+  let countryCode = document.getElementById("country").value;
+  console.log(countryCode);
+}
