3c88c6e4d6b2b29131e0961aee2680d1406f69fc
samiyusuf <samiyusuf@tamu.edu>
2022-04-06 14:26:58 -0500

    trying to get it to run on heroku

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-/*
+
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname })
-  res.sendFile('about.html', {root: __dirname })
 })
-*/
 
-app.use(express.static(__dirname + "/../csce315_project3"));
+
+//app.use(express.static(__dirname + "/../csce315_project3"));
 
 app.listen(port, () => {
   console.log(`TravelSight listening on port ${port}`)
