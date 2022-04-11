cdee2f85ddd4125e111f802d12fd83f33dcee31f
hamzasyed <hamzasyed@tamu.edu>
2022-04-11 10:04:35 -0500

    added date limiter

--- static/APIRequests.js
+++ static/APIRequests.js
@@ -203,3 +203,33 @@ async function getResult(){
     setTimeout(() => { getForecast(latlon[0], latlon[1]); }, 2500);
     //setTimeout(() => { getCurrencyConversion('USD', currencyCodes); }, 2000); 
 }
+
+function getCurrentDate(){
+    //https://stackoverflow.com/questions/32378590/
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+  
+    if (dd < 10) {dd = '0' + dd;}
+  
+    if (mm < 10) {mm = '0' + mm;} 
+      
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+  }
+  
+  function setMinEndDate(){
+    console.log("update min end day");
+    let minEndDay = document.getElementById("startDate").value;
+    document.getElementById("endDate").setAttribute("min", minEndDay);
+    document.getElementById("endDate").setAttribute("value", minEndDay);
+    console.log(minEndDay);
+    return minEndDay;
+  }
+  
+  function getCountryCode(){
+    let countryCode = document.getElementById("country").value;
+    console.log(countryCode);
+    return countryCode;
+  }
