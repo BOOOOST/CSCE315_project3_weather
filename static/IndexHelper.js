387cf9176d0eff0b6ff01401203148fc468c99ae
hamzasyed <hamzasyed@tamu.edu>
2022-04-12 12:05:59 -0500

    remove redundant code

--- static/IndexHelper.js
+++ static/IndexHelper.js
@@ -1,29 +1,30 @@
+//Functios to help with index page
 function getCurrentDate(){
     //https://stackoverflow.com/questions/32378590/
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-  
+
     if (dd < 10) {dd = '0' + dd;}
-  
+
     if (mm < 10) {mm = '0' + mm;} 
-      
+
     today = yyyy + '-' + mm + '-' + dd;
     return today;
-  }
-  
-  function setMinEndDate(){
-    console.log("update min end day");
+}
+
+//set the minimum end date to the start date to get a date range
+function setMinEndDate(){
     let minEndDay = document.getElementById("startDate").value;
     document.getElementById("endDate").setAttribute("min", minEndDay);
-    document.getElementById("endDate").setAttribute("value", minEndDay);
     console.log(minEndDay);
     return minEndDay;
-  }
+}
   
-  function getCountryCode(){
+//get the country code from dropdown
+function getCountryCode(){
     let countryCode = document.getElementById("country").value;
     console.log(countryCode);
     return countryCode;
-  }
+}
