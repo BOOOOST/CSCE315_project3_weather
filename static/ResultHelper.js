05e104832c09875b78b4adafe6a0e1b22e6c66c2
hamzasyed <hamzasyed@tamu.edu>
2022-04-19 14:31:23 -0500

    add results page

---
+++ static/ResultHelper.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    console.log("test");
+    document.getElementById("countryResult").innerHTML = localStorage.getItem("countryData");
+    document.getElementById("cityResult").innerHTML = localStorage.getItem("cityData");
+    document.getElementById("weatherResult").innerHTML = localStorage.getItem("weatherData");
+    document.getElementById("currencyResult").innerHTML = localStorage.getItem("currencyData");
+    
+}
