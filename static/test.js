82ee3152b2d8e76cea1b7a57519da9dcbf77f654
hamzasyed <hamzasyed@tamu.edu>
2022-04-21 12:57:46 -0500

    move big mac test to static

--- static/test.js
+++ static/test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const csv = require("csvtojson");
 
 async function csvStuff(countryCode){
-    const csvFilePath='C:/Users/hamza/Documents/A&M/CSCE315/csce315_project3/big-mac-full-index.csv'
+    const csvFilePath='C:/Users/hamza/Documents/A&M/CSCE315/csce315_project3/static/big-mac-full-index.csv'
     csv()
     .fromFile(csvFilePath)
     .then((jsonObj)=>{
@@ -10,7 +10,10 @@ async function csvStuff(countryCode){
         var data_filter = jsonObj.filter( element => element.iso_a3 == countryCode)
         data_filter = Object.values(data_filter);
         let bigMacData = data_filter[data_filter.length - 1];
-        console.log("Local price:",bigMacData.local_price, bigMacData.currency_code);
+        console.log("Local Price:",bigMacData.local_price, bigMacData.currency_code);
+        console.log("USD Price:",bigMacData.dollar_price, "USD");
+        console.log("GDP Adjusted Price:",bigMacData.adj_price, "USD");
+        console.log("Last Updated:",bigMacData.date);
     })
      
     // Async / await usage
